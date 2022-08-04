2068002228fee0556118d3cd6ce44febdc79b4fa
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-08-04 10:26:41 +0300

    function calls and declaration

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Follow along with the examples here
+
+/*
+function doNothing (){
+
+}
+function sayHell(){
+    console.log("Hello!");
+}
+
+function sayHelloToIsabel() {
+  console.log("Hello, Isabel!");
+}
+
+function sayHelloToSofia() {
+  console.log("Hello, Sofia!");
+}
+
+function sayHelloToBrendan() {
+  console.log("Hello, Brendan!");
+}
+
+sayHell();
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+*/
+// Using arguements and parameters in a function
+
+function sayHello(name) {
+  console.log(`Hello,${name}!`);
+}
+
+sayHello("Isabel");
